8bdb42d7bd42a5d2c7fe9d783099925a5d744ded
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-18 08:29:51 +0500

    initail commit

---
+++ script.js
@@ -0,0 +1,55 @@
+function makecard(projectLink, imgsorce, number, projecttitle) {
+    let col = document.createElement("div")
+    col.className = "col-lg-4"
+    document.getElementById("portfolio-container").append(col)
+
+    let cardoverlay = document.createElement("div")
+    cardoverlay.className = "card-overlayer"
+    col.append(cardoverlay)
+
+    let card = document.createElement("div")
+    card.className = "card"
+    cardoverlay.append(card)
+
+    let cardimg = document.createElement("div")
+    cardimg.className = "card-img"
+    card.append(cardimg)
+
+    let cardbody = document.createElement("div")
+    cardbody.className = "card-body"
+    card.append(cardbody)
+
+    let imglink = document.createElement("a")
+    imglink.setAttribute("target", "_blank")
+    imglink.setAttribute("href", projectLink)
+    cardimg.append(imglink)
+
+    let img = document.createElement("img")
+    img.setAttribute("src", imgsorce)
+    img.className = "card-img-top"
+    imglink.append(img)
+
+    let num = document.createElement("span")
+    num.innerText = number;
+    cardbody.append(num)
+
+    let title = document.createElement("p")
+    cardbody.append(title)
+
+
+    let imglink1 = document.createElement("a")
+    imglink1.setAttribute("href", projectLink)
+    imglink1.setAttribute("target", "_blank")
+
+    imglink1.innerText = projecttitle;
+    title.append(imglink1)
+
+
+
+}
+makecard("https://planview.netlify.app","images/plane.png","TOP # 1","PLanView Estate| Responsive Housing Scheme Website ")
+makecard("https://globallaw.netlify.app","images/global law.png","TOP # 2","GLobal Lawyer| Responsive Lawyer Hiring Company Website ")
+makecard("https://tastevibe.netlify.app","images/tastevibe.png","TOP # 3","TasteVibe| Responsive Resturant Fast Food And Drinks Website ")
+makecard("https://trippynow.netlify.app","images/trppy.png","TOP # 4","Trippy| Responsive Travling Journey Website ")
+
+
