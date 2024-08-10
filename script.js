ea202a1216dd3444dc6713abe4a1951d069149f3
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-08-10 20:04:12 +0500

    added new projects

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function makecard(projectLink, imgsorce, number, projecttitle) {
 
     let cardoverlay = document.createElement("div")
     cardoverlay.className = "card-overlayer"
-    cardoverlay.setAttribute("data-tilt-startX","20")
-    cardoverlay.setAttribute("data-tilt-startY","-20")
-    cardoverlay.setAttribute("data-tilt",null)
-    cardoverlay.setAttribute("data-tilt-reset-to-start","true")
+    if (screen.width >= 750) {
+        cardoverlay.setAttribute("data-tilt-startX", "20")
+        cardoverlay.setAttribute("data-tilt-startY", "-20")
+        cardoverlay.setAttribute("data-tilt", null)
+        cardoverlay.setAttribute("data-tilt-reset-to-start", "true")
+    }
 
     document.getElementById("portfolio-container").append(cardoverlay)
 
@@ -49,20 +51,22 @@ function makecard(projectLink, imgsorce, number, projecttitle) {
 
 
 }
-makecard("https://netflix1.netlify.app","images/netflix.png","TOP # 1","Netflix Clone| Netflix Clone with sign in page ")
-makecard("https://halal-spotify.freewebhostmost.com","images/spotify.png","TOP # 2","Halal Spotify| Spotify Clone with all features ")
-makecard("https://planview.netlify.app","images/plane.png","TOP # 3","PLanView Estate| Responsive Housing Scheme Website ")
-makecard("https://tastevibe.netlify.app","images/tastevibe.png","TOP # 4","TasteVibe| Responsive Resturant Fast Food And Drinks Website ")
-makecard("https://globallaw.netlify.app","images/global law.png","TOP # 5","GLobal Lawyer| Responsive Lawyer Hiring Company Website ")
-makecard("https://trippynow.netlify.app","images/trppy.png","TOP # 6","Trippy| Responsive Travling Journey Website ")
+makecard("https://huzareadify.netlify.app", "images/readify.png", "TOP # 1", "Readify| Personal book Library")
+makecard("https://halal-spotify.freewebhostmost.com", "images/spotify.png", "TOP # 2", "Halal Spotify| Spotify Clone with all features ")
+makecard("https://monopolyatm.netlify.app", "images/monopoly.png", "TOP # 3", "Monopoly Bank| Manage Monopoly Board Game Bank Website  ")
+makecard("https://huzatodolist.netlify.app", "images/todo.png", "TOP # 4", "Todo List App| Manage Daily Todos  ")
+makecard("https://planview.netlify.app", "images/plane.png", "TOP # 5", "PLanView Estate| Responsive Housing Scheme Website ")
+makecard("https://tastevibe.netlify.app", "images/tastevibe.png", "TOP # 6", "TasteVibe| Responsive Resturant Fast Food And Drinks Website ")
+makecard("https://globallaw.netlify.app", "images/global law.png", "TOP # 7", "GLobal Lawyer| Responsive Lawyer Hiring Company Website ")
+makecard("https://trippynow.netlify.app", "images/trppy.png", "TOP # 8", "Trippy| Responsive Travling Journey Website ")
 
 let menu = document.querySelector(".menu")
 let cross = document.querySelector(".cross")
 let menubox = document.querySelector(".menu-box")
 menu.addEventListener("click", (e) => {
-    
+
     menubox.style.display = "flex"
-   
+
 })
 cross.addEventListener("click", (e) => {
     menubox.style.display = "none"
